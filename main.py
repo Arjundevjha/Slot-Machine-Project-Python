54981f3a479752c7a57b665172440bbffe3509bd
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2023-08-01 04:35:06 +0000

    update

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def spin(balance):
     print_slot_machine(slots)
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
-    print(f"You won on lines:", *winning_lines)
+    print("You won on lines:", *winning_lines)
     return winnings - total_bet
 
 def main():
